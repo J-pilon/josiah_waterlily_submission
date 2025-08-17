0394d11998a8f7290da64f09e363165b11a557e0
Josiah <josiah.pilon@gmail.com>
2025-08-16 19:56:22 -0700

    feat: adds survey_submissions model with reference to survey

--- app/models/survey.rb
+++ app/models/survey.rb
@@ -1,3 +1,4 @@
 class Survey < ApplicationRecord
   has_many :questions, dependent: :destroy
+  has_many :survey_submissions, dependent: :destroy
 end
---
+++ app/models/survey_submission.rb
@@ -0,0 +1,3 @@
+class SurveySubmission < ApplicationRecord
+  belongs_to :survey
+end
